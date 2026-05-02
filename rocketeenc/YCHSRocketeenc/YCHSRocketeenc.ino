e5d10a3ec10d91beeaab2e2d09631c5011406f84
Jude Ausmus <jude.ausmus@gmail.com>
2026-05-01 22:45:58 -0700

    Move Accel read and Buzzer output to setup+devmode
    We only want to output the axis reading to Piezo on start up for
    self-test. Move code from loop to setup. While we're at it cleanup code
    and added DEVMODE for serial logging. :)
    
    Signed-off-by: Jude Ausmus <jude.ausmus@gmail.com>

--- rocketeenc/YCHSRocketeenc/YCHSRocketeenc.ino
+++ rocketeenc/YCHSRocketeenc/YCHSRocketeenc.ino
@@ -1,4 +1,9 @@
+
+
 #define BUZZPIN 4
+#define DEVMODE 1
+
+
 int ZACCELPIN = A6;
 
 int zAxis = 0;
@@ -6,25 +11,24 @@ int yAxis = 0;
 int xAxis = 0;
 
 void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(9600);
-  pinMode(BUZZPIN, OUTPUT);
-  //pinMode()
-}
+ 
 
-void loop() {
+  pinMode(BUZZPIN, OUTPUT);
   xAxis = analogRead(A6);
   yAxis = analogRead(A7);
   zAxis = analogRead(A8);
-  Serial.printf("Z: %i, Y: %i, X: %i\n", zAxis, yAxis, xAxis);
+  if (DEVMODE) {
+    Serial.begin(9600);
+    Serial.printf("Z: %i, Y: %i, X: %i\n", zAxis, yAxis, xAxis);
+  }
   tone(BUZZPIN, xAxis, 200);
   delay(500);
   tone(BUZZPIN, yAxis, 200);
   delay(500);
   tone(BUZZPIN, zAxis, 200);
-  delay(500);
-  //noTone(BUZZPIN);
-  //delay(500);
-  
+}
+
+void loop() {
+ 
 
 }
